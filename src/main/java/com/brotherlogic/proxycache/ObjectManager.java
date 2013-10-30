26141b76ef29c6e0f9a5c32fd68ec8368fc8d4e7
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2013-10-29 18:27:29 -0700

    Quick fix

--- src/main/java/com/brotherlogic/proxycache/ObjectManager.java
+++ src/main/java/com/brotherlogic/proxycache/ObjectManager.java
@@ -1,42 +1,50 @@
 package com.brotherlogic.proxycache;
 
-/**
- * Represents an object manager in the system. Object managers are able to process basic objects
- * @author simon
- *
- * @param <X> The generic class of this manager
- */
-public class ObjectManager<X> {
+import java.lang.reflect.Method;
+import java.net.URL;
 
-	/** The class defined by this manager */
-	private Class<X> underlyingClass;
-	
-	/** Flag to indicate if we want a fast (i.e. non updating) return */
-	private Boolean immediateReturn = false;
-	
-	/**
-	 * Base constructor
-	 * @param uClass The class that this object manages
-	 */
-	public ObjectManager(Class<X> uClass)
-	{
-		underlyingClass = uClass;
-	}
-	
-	public X get(String id)
-	{
-		//Check the local cache
-		
-		//Check for staling
-		
-		//Update the object if necessary
-		
-		//Return the object
-		return null;
-	}
-	
-	private void runMethod(Method m, URL url, String path)
-	{
-		
-	}
+/**
+ * Represents an object manager in the system. Object managers are able to
+ * process basic objects
+ * 
+ * @author simon
+ * 
+ * @param <X>
+ *           The generic class of this manager
+ */
+public class ObjectManager<X>
+{
+   /** Flag to indicate if we want a fast (i.e. non updating) return */
+   private final Boolean immediateReturn = false;
+
+   /** The class defined by this manager */
+   private final Class<X> underlyingClass;
+
+   /**
+    * Base constructor
+    * 
+    * @param uClass
+    *           The class that this object manages
+    */
+   public ObjectManager(Class<X> uClass)
+   {
+      underlyingClass = uClass;
+   }
+
+   public X get(String id)
+   {
+      // Check the local cache
+
+      // Check for staling
+
+      // Update the object if necessary
+
+      // Return the object
+      return null;
+   }
+
+   private void runMethod(Method m, URL url, String path)
+   {
+
+   }
 }
