94a4bca520b598ae6f3480c144841d907a7cdf42
Simon Tucker <brotherlogic@gmail.com>
2013-11-28 08:52:29 -0800

    Working on style stuff

--- src/main/java/com/brotherlogic/proxycache/WebList.java
+++ src/main/java/com/brotherlogic/proxycache/WebList.java
@@ -19,75 +19,67 @@ import com.google.gson.JsonObject;
  */
 public class WebList<X> extends UnboundedList<X> {
 
-	private final Pagination pagScheme;
-	private final String baseURL;
-	private final String path;
-	private JsonObject lastPull;
+    private final Pagination pagScheme;
+    private final String baseURL;
+    private final String path;
+    private JsonObject lastPull;
 
-	public WebList(Class<X> clz, StandardOAuthService serv, String baseURL,
-			String path, Pagination pag) {
-		super(clz, serv);
-		this.baseURL = baseURL;
-		this.path = path;
-		this.pagScheme = pag;
-	}
+    public WebList(Class<X> clz, StandardOAuthService serv, String url, String pathIn, Pagination pag) {
+        super(clz, serv);
+        this.baseURL = url;
+        this.path = pathIn;
+        this.pagScheme = pag;
+    }
 
-	@Override
-	protected int fillBottom() throws IOException {
-		// Get the builder
-		ObjectBuilder<X> builder = ObjectBuilderFactory.getInstance()
-				.getObjectBuilder(getUnderlyingClass(), getService());
+    @Override
+    protected int fillBottom() throws IOException {
+        // Get the builder
+        ObjectBuilder<X> builder = ObjectBuilderFactory.getInstance().getObjectBuilder(getUnderlyingClass(), getService());
 
-		System.out.println(pagScheme + " and " + lastPull);
-		System.out.println("Resolve = "
-				+ builder.resolvePath(pagScheme.botPath(), lastPull));
-		JsonElement elem = builder.resolvePath(pagScheme.botPath(), lastPull);
-		if (elem != null) {
-			String url = builder.resolvePath(pagScheme.botPath(), lastPull)
-					.getAsString();
-			lastPull = getService().get(url).getAsJsonObject();
-			JsonArray col = builder.resolvePath(path, lastPull)
-					.getAsJsonArray();
-			for (int i = col.size() - 1; i >= 0; i--) {
-				// Add to the bottom of the collection
-				X obj = builder.build(col.get(i).getAsJsonObject());
-				add(obj);
-			}
+        System.out.println(pagScheme + " and " + lastPull);
+        System.out.println("Resolve = " + builder.resolvePath(pagScheme.botPath(), lastPull));
+        JsonElement elem = builder.resolvePath(pagScheme.botPath(), lastPull);
+        if (elem != null) {
+            String url = builder.resolvePath(pagScheme.botPath(), lastPull).getAsString();
+            lastPull = getService().get(url).getAsJsonObject();
+            JsonArray col = builder.resolvePath(path, lastPull).getAsJsonArray();
+            for (int i = col.size() - 1; i >= 0; i--) {
+                // Add to the bottom of the collection
+                X obj = builder.build(col.get(i).getAsJsonObject());
+                add(obj);
+            }
 
-			return col.size();
-		}
+            return col.size();
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
-	@Override
-	protected int fillTop() throws IOException {
-		// Get the builder
-		ObjectBuilder<X> builder = ObjectBuilderFactory.getInstance()
-				.getObjectBuilder(getUnderlyingClass(), getService());
+    @Override
+    protected int fillTop() throws IOException {
+        // Get the builder
+        ObjectBuilder<X> builder = ObjectBuilderFactory.getInstance().getObjectBuilder(getUnderlyingClass(), getService());
 
-		lastPull = getService().get(baseURL).getAsJsonObject();
-		JsonArray col = builder.resolvePath(path, lastPull).getAsJsonArray();
-		for (int i = col.size() - 1; i >= 0; i--) {
-			// Add to the top of the collection
-			X obj = builder.build(col.get(i).getAsJsonObject());
-			add(0, obj);
-		}
+        lastPull = getService().get(baseURL).getAsJsonObject();
+        JsonArray col = builder.resolvePath(path, lastPull).getAsJsonArray();
+        for (int i = col.size() - 1; i >= 0; i--) {
+            // Add to the top of the collection
+            X obj = builder.build(col.get(i).getAsJsonObject());
+            add(0, obj);
+        }
 
-		return col.size();
-	}
+        return col.size();
+    }
 
-	public String generateBottomURL(JsonElement curr) {
-		if (!pagScheme.botPath().equals("")) {
-			return new ObjectBuilder<X>(getUnderlyingClass(), getService())
-					.resolvePath(pagScheme.botPath(), curr.getAsJsonObject())
-					.getAsString();
-		}
-		return "blah";
-	}
+    public String generateBottomURL(JsonElement curr) {
+        if (!pagScheme.botPath().equals("")) {
+            return new ObjectBuilder<X>(getUnderlyingClass(), getService()).resolvePath(pagScheme.botPath(), curr.getAsJsonObject()).getAsString();
+        }
+        return "blah";
+    }
 
-	public String generateTopURL(JsonElement curr) {
-		return "blah";
-	}
+    public String generateTopURL(JsonElement curr) {
+        return "blah";
+    }
 
 }
